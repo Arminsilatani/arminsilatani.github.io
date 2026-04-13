1bad40e78774707fd02dc450d7015a3706ec0241
Armin Silatani <59987007+Arminsilatani@users.noreply.github.com>
2026-04-13 07:30:20 +0330

    Update header-component.js

--- it/header-component.js
+++ it/header-component.js
@@ -255,9 +255,9 @@ class HeaderComponent extends HTMLElement {constructor() {
     const menu = document.createElement("nav");
     menu.className = "sidebar";
     menu.innerHTML = `
-      <a href="/arminsilatani.github.io/it/">Home</a>
+      <a href="/it/">Home</a>
       <a href="/it/services/">Servizi</a>
-      <a href="/arminsilatani.github.io/it/about/">Chi Sono</a>
+      <a href="/it/about/">Chi Sono</a>
       <a href="/it/contact/">Contattami</a>
       <a href="/it/tariff/">Tariffe</a>
       <a href="/it/portfolio/">Portfolio</a>
