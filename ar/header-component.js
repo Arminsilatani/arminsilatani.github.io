53b0a9c58496903bc25bda5a1a65215c5d99673f
Armin Silatani <59987007+Arminsilatani@users.noreply.github.com>
2026-04-13 07:33:16 +0330

    Change sidebar links to relative URLs
    Updated links in the sidebar to use relative paths.

--- ar/header-component.js
+++ ar/header-component.js
@@ -260,9 +260,9 @@ class HeaderComponent extends HTMLElement {
     const menu = document.createElement("nav");
     menu.className = "sidebar";
     menu.innerHTML = `
-      <a href="http://127.0.0.1:5500/arminsilatani.github.io/ar/">الرئيسية</a>
+      <a href="/ar/">الرئيسية</a>
       <a href="/ar/services">الخدمات</a>
-      <a href="http://127.0.0.1:5500/arminsilatani.github.io/ar/about/">من أنا</a>
+      <a href=/ar/about/">من أنا</a>
       <a href="/ar/contact">تواصل معي</a>
       <a href="/ar/tariff">الأسعار</a>
       <a href="/ar/portfolio">أعمالي</a>
