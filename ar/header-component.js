1e7f84e8f30ec1bf1824b4ce5f4066912376b26c
Armin Silatani <59987007+Arminsilatani@users.noreply.github.com>
2026-04-13 07:35:14 +0330

    Update header-component.js

--- ar/header-component.js
+++ ar/header-component.js
@@ -261,11 +261,11 @@ class HeaderComponent extends HTMLElement {
     menu.className = "sidebar";
     menu.innerHTML = `
       <a href="/ar/">الرئيسية</a>
-      <a href="/ar/services">الخدمات</a>
+      <a href="/ar/#">الخدمات</a>
       <a href=/ar/about/">من أنا</a>
-      <a href="/ar/contact">تواصل معي</a>
-      <a href="/ar/tariff">الأسعار</a>
-      <a href="/ar/portfolio">أعمالي</a>
+      <a href="/ar/#">تواصل معي</a>
+      <a href="/ar/#">الأسعار</a>
+      <a href="/ar/#">أعمالي</a>
     `;
 
     wrapper.append(lineBox, menu);
